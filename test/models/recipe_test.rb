8f478fc1ae8cd42c6d46321888911c5d0243f9d8
Michael Aldaba <maldaba3@gmail.com>
2015-11-21 14:11:39 -0500

    Create chefs and recipes associations

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "chicken parm", summary: "this is the best chicken parm recipe ever", 
+    @chef = Chef.create(chefname: "bob", email: "bob@example.com")
+    @recipe = @chef.recipes.build(name: "chicken parm", summary: "this is the best chicken parm recipe ever", 
               description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
   end
 
@@ -13,6 +14,11 @@ class RecipeTest < ActiveSupport::TestCase
 
   test "name should be present" do
     @recipe.name = nil
+    assert_not @recipe.valid?
+  end
+
+  test "chef_id should be present" do
+    @recipe.chef_id = nil
     assert_not @recipe.valid?
   end
 
